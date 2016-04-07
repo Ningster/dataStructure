9c356960aa111287a3f5598029a8a404e88b7e1b
Ningster <curarning@yahoo.com.tw>
2016-04-07 17:24:15 +0800

    add teacher's code as comment

--- sort/heap.py
+++ sort/heap.py
@@ -13,6 +13,20 @@ def adjust(a,i,n):   #a in a list of data, i is the root, n is the number of the
             j = 2*j
     a[j/2]=item
 
+"""
+    j=2*i
+    if j<=n:
+        if j<n and a[j]<a[j+1]:
+            j = j + 1
+        if a[i]>=a[j]:
+            pass
+        else:
+            temp = a[i]
+            a[i] = a[j]
+            a[j] = temp
+            adjust(a,j,n)
+"""
+
 def heapify(a,n):  #bottom up
     for i in range(n/2,0,-1):
         adjust(a,i,n)
@@ -26,6 +40,9 @@ def HeapSort(a,n):
         adjust(a,1,i-1)
 
 
-listA=[0,77,5,20,1,61,11,59,15,48,19]
+listA=[0,11,5,20,1,61,15,59,77,48,19]
 HeapSort(listA,10)
 print listA
+
+#adjust(listA,1,10)
+#print listA
